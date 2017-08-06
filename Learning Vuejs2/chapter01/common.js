85590953097e651038e226431456bea9a31b81a0
jung <jnoah80@gmail.com>
2017-08-06 23:46:05 +0900

    Learning Vue.js 2 / chapter01 / 30% / 이제 시작함 ㅠㅠ

---
+++ Learning Vuejs2/chapter01/common.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    function onAdd() {
+        var $ul, li, $li, $label, $div, value;
+
+        value = $('.js-new-item').val();
+
+        if (value === ''){
+            return;
+        }
+
+        $ul = $('ul');
+        $li = $('<li>').appendTo($ul);
+        $div = $('<div>').addClass('checkbox').appendTo($li);
+        $label = $('<label>').appendTo('$div');
+        $('<input>').attr('type', 'checkbox').addClass('item').click(toggleRemoved).appendTo($label);
+
+        $label.append(value);
+        $('.js-new-item').val('');
+    }
+
+    function toggleRemoved(ev) {
+        var $el;
+
+        $el = $(ev.currentTarget);
+        $el.closest('li').toggleClass('removed');
+    }
+
+    $('.js-add').click(onAdd);
+    $('.js-item').click(toggleRemoved);
+});
