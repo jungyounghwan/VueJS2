62c94d24f13573be94f779865a164c1b41d48c8e
jung <jnoah80@gmail.com>
2017-08-28 22:30:05 +0900

    최초커밋

---
+++ Learning Vuejs2/chapter03/ex4-webpack-simple/shopping-list/src/components/ItemsComponent.vue
@@ -0,0 +1,18 @@
+<!--
+<template id="items-template">
+    <ul>
+        <item-component v-for="item in items" :item="item"></item-component>
+    </ul>
+</template>
+
+<script>
+    import ItemComponent from './ItemComponent.vue'
+
+    export default {
+        components : {
+            ItemComponent
+        },
+        props : ['items']
+    }
+</script>
+-->
